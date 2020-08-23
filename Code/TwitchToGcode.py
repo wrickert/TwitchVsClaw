1d0615ce344e50019844dc0b32f5db1077b96180
William Rickert <brickert7@gmail.com>
2020-08-23 18:23:48 -0500

    More brackets

--- Code/TwitchToGcode.py
+++ Code/TwitchToGcode.py
@@ -2,17 +2,34 @@ import twitch
 import serial
 import time
 import config
+import io
+
+Xpos = 0
+Ypos = 0
+Zpos = 0
 
 class SerialWrapper:
     def __init__(self, device):
         self.ser = serial.Serial(device, 115200)
+        self.io = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser), newline='\n')
+        Xpos = 0
+        Ypos = 0
+        Ypos = 0
 
     def sendData(self, data):
         data += "\r\n"
         self.ser.write(data.encode())
 
+    def readline(self):
+         print("before")
+         return self.io.readline(1)
+         print("after")
 
 def handle_message(message: twitch.chat.Message) -> None:
+   global Xpos 
+   global Ypos 
+   global Zpos  
+
    #Start with the help message
    if message.text.startswith('!help'):
       message.chat.send("Test")
@@ -29,19 +46,37 @@ def handle_message(message: twitch.chat.Message) -> None:
       ser.sendData('M107')   
 
    if message.text.startswith('!up'):
-      ser.sendData('G0 Z10')
+      Zpos += 10
+      ser.sendData('G0 Z' + str(Zpos))
 
    if message.text.startswith('!down'):
-      ser.sendData('G0 Z-10')
+      ser.sendData('G0 Z10')
 
    if message.text.startswith('!forward'):
-      ser.sendData('G0 Y10')
+      Ypos += 50
+      ser.sendData('G0 Y' + str(Ypos))
 
    if message.text.startswith('!back'):
-      ser.sendData('G0 Y-10')
+      Ypos -= 50
+      ser.sendData('G0 Y' + str(Ypos))
+
+   if message.text.startswith('!right'):
+      Xpos += 50
+      ser.sendData('G0 X' + str(Xpos))
+      print(Xpos)
+
+   if message.text.startswith('!left'):
+      Xpos -= 50
+      ser.sendData('G0 X' + str(Xpos))
+      print(Xpos)
+
+   if message.text.startswith('!where'):
+      ser.sendData('M114')
+      message.chat.send(ser.readline())
 
 def main():
    global ser
+
    ser = SerialWrapper('/dev/ttyUSB0')
 
 #   ser.sendData('G92 100 100 100')
